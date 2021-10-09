4057a4634f2c7de168df8177c731c9f84f70d860
sebastian merz <62221161+MerzSebastian@users.noreply.github.com>
2021-10-09 11:06:55 +0200

    cleaning up, getting it back to work

--- src/decode.py
+++ src/decode.py
@@ -3,8 +3,7 @@
 # Description: Script for decoding digital signal from a single or multiple CSV files which are expored from a Siglent oscilloscope (bin dump => bin2csv)
 # Parameter: decode.py <input_file_or_folder> <output_folder>
 # Example: decode.py ../test/samples ./
-import datetime as datetime
-import pandas as pd
+
 import sys
 import time
 import os as os
@@ -29,10 +28,6 @@ def decode_normalized_data(val_x, val_y, single_pulse_length):
     return data, error
 
 
-def normalize_data():
-    print("test")
-
-
 def reformat_raw_data(data):
     x = [data[0][i][0] for i in range(len(data[0]))]
     y = [data[0][i][1] for i in range(len(data[0]))]
@@ -40,61 +35,81 @@ def reformat_raw_data(data):
     return x, y
 
 
+def remove_redundant_data_points(x, y):
+    #x = [([x[i], x[i]]) for i in range(1, len(x) - 1) if (y[i - 1] != y[i]) and (y[i + 1] == y[i])]
+    #y = [([1 - y[i], y[i]]) for i in range(1, len(x) - 1) if (y[i - 1] != y[i]) and (y[i + 1] == y[i])]
+    xx = []
+    yy = []
+    for i in range(1, len(x) - 1):
+        if y[i - 1] != y[i] and y[i + 1] == y[i]:
+            xx.extend([x[i], x[i]])
+            yy.extend([1 - y[i], y[i]])
+    return xx, yy
+
+
+def write_output(path, data):
+    name = str(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")) + ".json"
+    _path = os.path.join(path, name)
+    f = open(_path, "x")
+    f.write(json.dumps(data))
+    f.close()
+    return name
+
+start_time = 0
+last_time = 0
+
+def time_round():
+    global last_time
+    print("Elapsed time:", round((time.perf_counter() - last_time)*1000), "ms")
+    last_time = time.perf_counter()
+
+def time_start():
+    global last_time
+    global start_time
+    last_time = time.perf_counter()
+    start_time = time.perf_counter()
+
+def time_overall():
+    global start_time
+    print("Overall elapsed time:", round((time.perf_counter() - start_time) * 1000), "ms")
+
 def main():
+    time_start()
+
     filePaths = ([os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1]) if f.endswith('.bin')] if os.path.isdir(sys.argv[1]) else [sys.argv[1]])
 
     result = {}
-    lastTime = time.perf_counter()
-    startTime = time.perf_counter()
     for file in filePaths:
         print("# # # # # # # # # # # # # # # # # # # # # #")
-        print("Reading file", file, "...")
-
-        xx = []
-        yy = []
-        data = bin(file).convert()[0]
-
-        x, y = reformat_raw_data(bin(file).convert()[0])
-        xx = x
-        yy = y
-
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        print("Reading and converting binary data", file, "...")
+        raw_data = bin(file).convert()[0]
+        x, y = reformat_raw_data(raw_data)
+        time_round()
 
         print("Calculating threshold and cleaning data...")
-        threshold = max(yy) / 2
-        yy = [(True if i > threshold else False) for i in yy]
+        threshold = max(y) / 2
+        y = [(True if i > threshold else False) for i in y]
         print("Threshold:", threshold, "Volt")
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        time_round()
 
         print("Removing doubled data points...")
-        x = []
-        y = []
-        for i in range(1, len(xx) - 1):
-            if yy[i - 1] != yy[i] and yy[i + 1] == yy[i]:
-                x.extend([xx[i], xx[i]])
-                y.extend([1 - yy[i], yy[i]])
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        x, y = remove_redundant_data_points(x, y)
+        time_round()
 
         print("Shifting time value so it starts at 0...")
         x = [x[i] - min(x) for i in range(len(x))]
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        time_round()
 
         print("Getting shortest pulse length...")
         singlePulseLength = min([abs(x[i] - x[i + 2]) for i in range(len(y) - 2)])
         print("Shorted pulse length:", singlePulseLength)
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        time_round()
 
         print("Decoding data...")
         decoded_data, gError = decode_normalized_data(x, y, singlePulseLength)
         decoded_string_data = ''.join(str(val) for val in decoded_data)
         print("Decoded data:", decoded_string_data)
-        print("Elapsed time:", round((time.perf_counter() - lastTime)*1000), "ms")
-        lastTime = time.perf_counter()
+        time_round()
 
         #THROW ERROR IF length of < and x are not the same
 
@@ -102,7 +117,7 @@ def main():
             "date": datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
             "detectionThreshold": format(threshold, '.12f'),
             "singlePulseLength": format(singlePulseLength, '.12f'),
-            "dataPointsRaw": len(data[0]),
+            "dataPointsRaw": len(raw_data[0]),
             "maxError": format(max(gError), '.12f'),
             "avgError": format(statistics.mean(gError), '.12f'),
             "data": {
@@ -115,12 +130,10 @@ def main():
 
     print("# # # # # # # # # # # # # # # # # # # # # #")
     print("Writing output file...")
-    path = os.path.join(sys.argv[2], str(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")) + ".json")
-    f = open(path, "x")
-    f.write(json.dumps(result))
-    f.close()
+    path = write_output(sys.argv[2], result)
+
     print("Written file:", path)
-    print("Overall time:", round((time.perf_counter() - startTime)*1000), "ms")
+    time_overall()
     print("# # # # # # # # # # # # # # # # # # # # # #")
 
 if __name__ == "__main__":
